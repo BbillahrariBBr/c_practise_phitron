325e27abb337d126928ae306ec6776994cf0a2eb
BbillahrariBBr <mb.billah8@gmail.com>
2022-04-11 21:34:08 +0600

    2.5.5 done

---
+++ Module_2.5_practice/2.5_5.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main(){
+
+    int Int=5;
+    printf("%d\n",Int);
+    int Calloc=5;
+    printf("%d\n",Calloc);
+    int floating=5;
+    printf("%d\n",floating);
+    int ReInitialize=5;
+    printf("%d\n",ReInitialize);
+    //int char=5; wrong
+    //printf("%d\n",char);
+
+    int Xx=5;
+    printf("%d\n",Xx);
+
+    int _1312=6;
+    printf("%d\n",_1312);
+    int _=7;
+    printf("%d\n",_);
+
+    //int 6_05=8; wrong
+    //printf("%d\n",6_05);
+
+    int alpha_beta_routine=8;
+    printf("%d\n",alpha_beta_routine);
+
+    int z=7;
+    printf("%d\n",z);
+
+    int A$=100;
+    printf("%d\n",A$);
+    return 0;
+
+
+}
