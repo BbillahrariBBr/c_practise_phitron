5841602b7925be56c87ed0881dfd5acae9f800e0
BbillahrariBBr <mb.billah8@gmail.com>
2022-04-16 23:34:42 +0600

    calcultor without switch

---
+++ Module_4/calculattor without switch.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int num1, num2;
+    char op;
+    int result;
+    scanf("%d %c %d", &num1,&op,&num2);
+    if(op == '+' ){
+        result = num1+ num2;
+        printf("%d", result);
+    }
+    else if(op == '-' ){
+        result = num1- num2;
+        printf("%d", result);
+    }
+    else if(op == '*' ){
+        result = num1* num2;
+        printf("%d", result);
+    }
+    else if(op == '/' ){
+        result = num1 / num2;
+        printf("%d", result);
+    }
+
+    return 0;
+}
