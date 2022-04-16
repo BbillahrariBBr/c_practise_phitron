8615f3b029417aaaa29b9b4d77257434939fc1d2
BbillahrariBBr <mb.billah8@gmail.com>
2022-04-16 23:58:41 +0600

    problem sample

---
+++ Module_4/problem test.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main() {
+	int value = -23;
+	if (value > 0) {
+    	printf("A ");
+    	if (value % 5 == 0) {
+        	printf("B ");
+    	}
+    	else {
+        	printf("C ");
+    	}
+	}
+	else {
+    	printf("D ");
+    	if (value % 5 == 0) {
+        	printf("E ");
+    	}
+    	else {
+        	printf("F ");
+    	}
+	}
+}
