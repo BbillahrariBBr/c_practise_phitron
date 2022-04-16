cefba0bd9f985220f496a85db35ddad2a9d441e1
BbillahrariBBr <mb.billah8@gmail.com>
2022-04-16 23:02:06 +0600

    relational

---
+++ Module_4/relationla.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main()
+{
+    int comp = 0;
+    bool test =true;
+    printf("%d\n",test);
+
+    if(comp){
+        printf("true");
+    }
+    else{
+        printf("false");
+    }
+}
