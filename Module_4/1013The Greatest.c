df270466c508001c637c172f534d2ad1a8e21082
BbillahrariBBr <mb.billah8@gmail.com>
2022-04-18 16:43:14 +0600

    1013 uri online problem solved

--- Module_4/1013The Greatest.c
+++ Module_4/1013The Greatest.c
@@ -1,1 +1,14 @@
+// reads 3 int value and present one followed by the message "eh o mario"
 
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int a,b,c;
+    scanf("%d %d %d", &a,&b,&c);
+    int mxAb = ((a+b)+(abs(a-b)))/2;
+    int mxAbc = ((mxAb+c)+(abs(mxAb-c)))/2;
+    printf("%d eh o maior\n",mxAbc);
+    return 0;
+}
