6bef7ba6e288c71a14a037e14d035ce430c31fb3
BbillahrariBBr <mb.billah8@gmail.com>
2022-04-18 17:54:54 +0600

    10119 uri online problem solved

---
+++ Module_4/1019UriTime Conversion.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int h = n/3600;
+    int m = (n%3600)/60;
+    int s = n - (h*3600) - (m*60);
+    printf("%d:%d:%d\n",h,m,s);
+    return 0;
+}
