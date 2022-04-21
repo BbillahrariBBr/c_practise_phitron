ec12edf1c88dc7dc254325f2d1c131d4d1b4b878
BbillahrariBBr <mb.billah8@gmail.com>
2022-04-22 00:30:53 +0600

    add labxm01_h

---
+++ Lab Exam 01/labxm_01_h.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int A, B;
+    scanf("%d %d",&A,&B);
+    if(A+B>=10)
+    {
+        printf("error\n");
+    }
+    else
+    {
+        printf("%d\n",A+B);
+    }
+    return 0;
+}
