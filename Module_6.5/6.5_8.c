f019f75465b7e83d44259de9e4e8513b95e41dbc
BbillahrariBBr <mb.billah8@gmail.com>
2022-04-21 13:26:27 +0600

    6.5_8 done

---
+++ Module_6.5/6.5_8.c
@@ -0,0 +1,21 @@
+/*
+
+Write a C program to check whether an alphabet is a vowel or consonant
+
+*/
+
+#include<stdio.h>
+int main()
+{
+    char c;
+    scanf("%c", &c);
+
+    if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+    {
+        printf("vowel");
+    }
+    else{
+        printf("consonant");
+    }
+    return 0;
+}
