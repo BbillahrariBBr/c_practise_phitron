e44c69fca3b3b871207f425ade78551ae387e05b
BbillahrariBBr <mb.billah8@gmail.com>
2022-04-23 21:08:20 +0600

    while xmpl 4 gcd 2

--- Module_8/whileex5 GCD2.c
+++ Module_8/whileex5 GCD2.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    // using for loop 30 42
+    int a,b,x,gcd = 1;
+    scanf("%d %d",&a,&b);
+    while(a!=0)
+    {
+        int rem = b%a;
+        b = a;
+        a = rem;
+        printf("%d %d\n",a,b);
+    }
+    printf("GCD is: %d\n",b);
+    return 0;
+}
 
