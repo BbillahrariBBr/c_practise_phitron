774eade714482cba59fc19b0acb93ace2bafe009
BbillahrariBBr <mb.billah8@gmail.com>
2022-04-26 00:59:40 +0600

    9.5 done

--- module9.5/9.5_I - Grading Students.c
+++ module9.5/9.5_I - Grading Students.c
@@ -1,1 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
 
+    for(int i =1; i<=n; i++)
+    {
+        int grade;
+        scanf("%d",&grade);
+        if(grade < 38)
+        {
+            printf("%d\n",grade);
+        }
+        else if(5-(grade%5)<3)
+        {
+            printf("%d\n",5*(grade/5 +1));
+        }
+        else{
+            printf("%d\n",grade);
+        }
+    }
+    return 0;
+}
