b34d49ec594ec79875d9d36a1b06630bf750e7cd
BbillahrariBBr <mb.billah8@gmail.com>
2022-04-25 21:34:08 +0600

    9.5_3b done

--- module9.5/9.5_3_b.c
+++ module9.5/9.5_3_b.c
@@ -1,1 +1,17 @@
-
+#include<stdio.h>
+int main()
+{
+    for(int i=1; i<=100; i++){
+        if(i%3==0) continue;
+        printf("%d\n",i);
+    }
+//    int i =1;
+//    while(i<=100){
+//        if(i%2==0) continue;
+//
+//        printf("%d\n",i);
+//
+//        i++;
+//    }
+    return 0;
+}
