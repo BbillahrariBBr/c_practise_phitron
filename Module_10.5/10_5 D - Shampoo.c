7f307debccfddef001e24510d1b4c8e9a32afe2f
BbillahrariBBr <mb.billah8@gmail.com>
2022-04-27 21:46:17 +0600

    module_10.5_D done

---
+++ Module_10.5/10_5 D - Shampoo.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int v,f,m,t;
+    scanf("%d %d %d %d",&v, &f, &m, &t);
+    int total = f+m+t;
+
+    int shampoo = v % total;
+    if(shampoo >= 0 && shampoo <f)
+    {
+        printf("F");
+    }
+
+    else if(shampoo >= f && shampoo <f+m)
+    {
+        printf("M");
+    }
+
+    else
+    {
+        printf("T");
+    }
+}
