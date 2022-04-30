bb0221f2cdbc4f6f457a48cf31d3a80b8d50f10d
BbillahrariBBr <mb.billah8@gmail.com>
2022-05-01 01:03:06 +0600

    lab 02 f done

---
+++ Lab Exam 02/F - F.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+int main()
+{
+    int n,m;
+    int count =1;
+    scanf("%d %d",&n,&m);
+    if(n%2 && (n>=3 && n<=50) && (m>=3 && m<=50))
+    {
+        for(int i=1;i<=n; i++)
+        {
+            for(int j =1;j<=m; j++)
+            {
+                if(i%2){
+                    printf("#");
+                }
+                else
+                {
+                    if(m%2)
+                    {
+                        if(count%2  && j == m)
+                        {
+                            printf("#");
+
+                        }
+                        else if(count%2 == 0 && j ==1)
+                        {
+                            printf("#");
+
+
+                        }
+                        else
+                        {
+                           printf(".");
+                        }
+                        count++;
+
+                    }
+                    else
+                    {
+
+                        if(count%2==0  && j == m && (i/2)%2)
+                        {
+                            printf("#");
+
+                        }
+                        else if(count%2 && j ==1 && (i/2)%2 ==0)
+                        {
+                            printf("#");
+                            //count++;
+
+
+                        }
+                        else
+                        {
+                           printf(".");
+                        }
+                        count++;
+
+
+                    }
+
+
+                }
+            }
+            printf("\n");
+        }
+
+    }
+
+    return 0;
+}
