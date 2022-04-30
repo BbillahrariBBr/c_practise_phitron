f62e01ba9b81a37b05d4b76c67ab9435c139dfdd
BbillahrariBBr <mb.billah8@gmail.com>
2022-05-01 01:47:40 +0600

    lab 02 submission done

--- Lab Exam 02/A - A.c
+++ Lab Exam 02/A - A.c
@@ -2,11 +2,23 @@
 int main()
 {
     int N,K,A;
+    int count;
     scanf("%d %d %d", &N, &K, &A);
 
     if(A>= 1 && A<=N)
     {
+        for(int i =2; i<=K; i++)
+        {
+            if(A==N)
+            {
+                A =1;
+            }
+            else{
+                A +=1;
+            }
+        }
 
     }
+    printf("%d\n",A);
     return 0;
 }
