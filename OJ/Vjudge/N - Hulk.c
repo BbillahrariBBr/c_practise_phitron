46a6cd9e25e48ab6e8ce09e62af8b49657aa265a
BbillahrariBBr <mb.billah8@gmail.com>
2022-05-02 10:51:49 +0600

    bracu beginner problem solve n

---
+++ OJ/Vjudge/N - Hulk.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    //previous solve
+    int n;
+    scanf("%d",&n);
+    for(int i=1; i<=n; i++)
+    {
+        if(i%2)
+        {
+            printf("I hate ");
+        }
+        else
+        {
+            printf("I love ");
+        }
+
+        if(i== n)
+        {
+            printf("it");
+        }
+        else
+        {
+            printf("that ");
+        }
+    }
+    return 0;
+}
