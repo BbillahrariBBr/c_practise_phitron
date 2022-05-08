f41bb2b3aae4d335f474b15d425ff83a8e11ad6f
BbillahrariBBr <mb.billah8@gmail.com>
2022-05-08 07:08:01 +0600

    array basic A done

---
+++ OJ/Vjudge/A - Max element in the array.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i =0; i<n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    long long max = arr[0];
+    for(int i =1; i<n;i++)
+    {
+        if (max<arr[i])
+        {
+            max = arr[i];
+        }
+    }
+    printf("%lld\n",max);
+    return 0;
+}
--- OJ/Vjudge/E - Find duplicate element in array .c
+++ OJ/Vjudge/E - Find duplicate element in array .c
@@ -10,17 +10,15 @@ int main()
     }
     for(int i =0; i<n;i++)
     {
-        for(int j=0; j<n;j++)
+        for(int j=i+1; j<n;j++)
         {
-            if(i!=j)
+            if(arr[i]==arr[j])
             {
-                if(arr[i]==arr[j])
-                {
-                    printf("%d\n",arr[i]);
-                    break;
-                }
+                printf("%d\n",arr[i]);
+                break;
             }
         }
     }
+
     return 0;
 }
