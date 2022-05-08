8d5d0b94c39b3ff11c27d0456d78c3c5e2c5dccf
BbillahrariBBr <mb.billah8@gmail.com>
2022-05-08 08:26:18 +0600

    B done

--- OJ/Vjudge/A - Max element in the array.c
+++ OJ/Vjudge/A - Max element in the array.c
@@ -3,16 +3,16 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    int arr[n];
+    long long arr[n];
     for(int i =0; i<n; i++)
     {
-        scanf("%d",&arr[i]);
+        scanf("%lld",&arr[i]);
     }
 
     long long max = arr[0];
     for(int i =1; i<n;i++)
     {
-        if (max<arr[i])
+        if (arr[i]>max)
         {
             max = arr[i];
         }
