2759a7959f65f2cbe8c9c65df06afa5e87bb81b9
BbillahrariBBr <mb.billah8@gmail.com>
2022-05-10 20:52:29 +0600

    14_1_array reverse with kindu code

--- Module_14_Lab_04/1_array reverse.c
+++ Module_14_Lab_04/1_array reverse.c
@@ -7,11 +7,31 @@ int main()
     {
         scanf("%d",arr+i);
     }
-    for(i=size-1,j=0;i>=0;i--,j++)
+//    for(i=size-1,j=0;i>=0;i--,j++)
+//    {
+//        rev[j] = arr[i];
+//        printf("%d\t",rev[j]);
+//    }
+
+    for(i=size-1,j=0;j<i;i--,j++)
     {
-        rev[j] = arr[i];
-        printf("%d\t",rev[j]);
+        int temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] =temp;
+
     }
+//    for(i=0;i<size;i++)
+//    {
+//        rev[size-1-i] = arr[i];
+//
+//    }
+     for(i=0;i<size;i++)
+    {
+//        printf("%d\t",rev[i]);
+        printf("%d\t",arr[i]);
+
+    }
+
 
     return 0;
 }
