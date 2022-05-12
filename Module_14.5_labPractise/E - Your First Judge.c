3f76d5c097d301c226d7eb6e3f5a5c126a3887eb
BbillahrariBBr <mb.billah8@gmail.com>
2022-05-12 13:39:18 +0600

    add e done

---
+++ Module_14.5_labPractise/E - Your First Judge.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[16];
+    char t[] ="Hello,World!";
+    scanf("%s",s);
+    int temp = strcmp(s,t);
+    if(temp==0)
+    {
+        printf("AC\n");
+
+        return 0;
+    }
+    printf("WA\n");
+
+    return 0;
+}
