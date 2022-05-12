e17de184a64ceb65d4a748c1022449ceeea93457
BbillahrariBBr <mb.billah8@gmail.com>
2022-05-13 01:58:39 +0600

    lab midterm B total increase

---
+++ Module_15 Midterm/B - B.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+//abs();
+int main()
+{
+    int n,inc=0;
+    scanf("%d",&n);
+    int arr[100];
+    int max =0;
+    for(int i =0; i<n; i++)
+    {
+        scanf("%d",arr+i);
+        if(arr[i]>max)
+        {
+            max= arr[i];
+        }
+    }
+    for(int i =0; i<n; i++)
+    {
+        inc = inc +(max-arr[i]);
+
+    }
+    printf("%d\n",inc);
+
+    return 0;
+}
