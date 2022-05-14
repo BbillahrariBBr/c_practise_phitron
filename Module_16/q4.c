f32e4bd8ee9a3936f10c4a3ef0aefedc01ead902
BbillahrariBBr <mb.billah8@gmail.com>
2022-05-14 22:14:38 +0600

    module 16 q4

---
+++ Module_16/q4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a[10];
+    printf("%d\n", a[0]);
+    a[0]+=10;
+    printf("%d\n", a[0]);
+    a[0]= a[0]*2;
+    printf("%d\n", a[0]);
+    return 0;
+}
