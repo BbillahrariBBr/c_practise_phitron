865537c1419af47c4a3d8e096e28725df9a3b142
BbillahrariBBr <mb.billah8@gmail.com>
2022-05-18 07:10:19 +0600

    practise day 3 done

--- Module_17.5_practice/3Array taken func and find max.c
+++ Module_17.5_practice/3Array taken func and find max.c
@@ -1,9 +1,32 @@
 #include<stdio.h>
+#include<limits.h>
+int maxNumber(int a,int b)
+{
+    if(a>b) return a;
+    else    return b;
+}
+getMax(int n, int arr[])
+{
+    int max = INT_MIN;
+    for(int i=0; i<n; i++)
+    {
+        max = maxNumber(max,arr[i]);
+    }
+    return max;
+
+}
+
 int main()
 {
     int n;
     printf("Enter the size of Array: ");
     scanf("%d",&n);
-
+    int arr[n];
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d",arr+i);
+    }
+    int max = getMax(n,arr);
+    printf("%d",max);
 
 }
--- Module_17.5_practice/function 1 conceptual.c
+++ Module_17.5_practice/function 1 conceptual.c
@@ -3,17 +3,19 @@ int main()
 {
     int a = 10;
     int *pa = &a;
-    int *pAA = &pa;
+    int **pAA = &pa;
 
     printf("A er value- %d\n",a);
     printf("A er  addres- %d\n",&a);
     printf("p er  Value(a er address)- %d\n",pa);
     printf("p er  Adress er balueValue(a er value)- %d\n",*pa);
-    printf("p er  Adress- %d",&pa);
+    printf("p er  Adress- %d\n",&pa);
 
-    printf("pAA er  Value(a er address)- %d\n",pAA);
-    printf("pAA er  Adress er balueValue(a er value)- %d\n",*pAA);
+    printf("pAA er  Value(pa er address)- %d\n",pAA);
+    printf("pAA er  Adress er Value(pa er value)- %d\n",*pAA);
     printf("pAA er  Adress- %d\n",&pAA);
+
+     printf("pAA er  Adress er Value(p er  Adress er balueValue(a er value))- %d\n",**pAA);
 
 
 
