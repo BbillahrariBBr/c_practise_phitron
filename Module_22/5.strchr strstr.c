c77715f5945326e5e83e2789b9631a28951225e7
BbillahrariBBr <mb.billah8@gmail.com>
2022-05-25 00:44:10 +0600

    22 done

---
+++ Module_22/5.strchr strstr.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[] ="Hello";
+
+
+    return 0;
+}
+
+/**
+
+for(int i =0; i<6; i++)
+    {
+        printf("%c %d\n",str[i],str[i]);
+    }
+
+    printf("%s",str+1);
+
+*/
