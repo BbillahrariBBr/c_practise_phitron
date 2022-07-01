a7468f070acaac32c6cacd5d2553703de2bb1815
BbillahrariBBr <mb.billah8@gmail.com>
2022-07-01 14:18:34 +0600

    final theory 1 done

---
+++ lab exam 9 final/1Theory1.c
@@ -0,0 +1,9 @@
+void max(int x, int y)        // function definition
+{
+    if(x > y) {
+        printf("The greater number is: %d", x);
+    }
+    else {
+        printf("The greater number is: %d", y);
+    }
+}
