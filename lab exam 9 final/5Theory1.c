54702792dec17462123b868a0b1e3131e89163f1
BbillahrariBBr <mb.billah8@gmail.com>
2022-07-01 17:03:06 +0600

    final theory 5 done

---
+++ lab exam 9 final/5Theory1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char st[1000];
+    scanf("%s",st);
+    int counter[10] ={0};
+
+    for(int i =0; i<strlen(st); i++)
+    {
+
+        char c = st[i];
+        if(c >= '0' && c <= '9')
+        {
+            int digit = c-'0';
+            counter[digit]++;
+        }
+
+    }
+
+    for(int i =0; i<=9; i++)
+    {
+        if(counter[i]==0)
+        {
+            printf("No\n");
+            return 0;
+        }
+    }
+    printf("Yes\n");
+
+    return 0;
+}
+
