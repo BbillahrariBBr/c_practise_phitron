0294fb73f9092e3bbc86212315873f8dcfb9d94b
BbillahrariBBr <mb.billah8@gmail.com>
2022-07-01 20:51:38 +0600

    final theory 9 done

---
+++ lab exam 9 final/9Theory1.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+struct student
+{
+    int roll;
+    int clss;
+    int marks;
+
+};
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    struct student stu[n];
+    int i;
+    for(i=0; i<n;i++)
+    {
+        printf("Student %d\n",i+1);
+        printf("Enter roll no.: ");
+        scanf("%d", &stu[i].roll);
+        printf("Enter class : ");
+        scanf("%s",&stu[i].clss);
+        printf("Enter marks : ");
+        scanf("%d", &stu[i].marks);
+    }
+
+    int sum = 0;
+    for(i=0; i<n; i++)
+    {
+        sum +=stu[i].marks;
+    }
+
+    printf("\n%d\n",sum);
+
+    return 0;
+}
