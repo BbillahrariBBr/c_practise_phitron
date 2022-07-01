1e7d54634d0e68ff47d4a4a744a1214a9c542451
BbillahrariBBr <mb.billah8@gmail.com>
2022-07-01 11:48:33 +0600

    final j done

---
+++ lab exam 9 final/Jcontest.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int n, comp=0;
+    scanf("%d",&n);
+    getchar();
+
+    char name[n][21];
+    for(int i = 0; i<n; i++)
+    {
+        gets(name[i]);
+        for(int j=0; j<i ; j++)
+        {
+            if(strcmp(name[i], name[j])==0)
+                comp=1;
+
+        }
+    }
+    if(comp)
+        printf("Yes\n");
+    else
+        printf("No\n");
+
+    return 0;
+}
